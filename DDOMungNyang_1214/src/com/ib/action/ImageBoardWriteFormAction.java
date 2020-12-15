7c6005a8d3e61cb0d507afc067ad77aea73bda37
kunwoo <pigonhair@gmail.com>
2020-12-15 14:23:10 +0900

    1

---
+++ DDOMungNyang_1214/src/com/ib/action/ImageBoardWriteFormAction.java
@@ -0,0 +1,14 @@
+package com.ib.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ImageBoardWriteFormAction implements CommandProcess{
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		
+		return null;
+	}
+
+}

3cb11f4b9143bf8f42a6b5001332207878e022d0
kunwoo <pigonhair@gmail.com>
2020-12-16 17:48:18 +0900

    강아지 상세페이지(수정하기 기능)
    :wq

--- DDOMungNyang_1215/src/com/ib/action/CommandProcess.java
+++
@@ -1,8 +0,0 @@
-package com.ib.action;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface CommandProcess {
-	String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable;
-}
